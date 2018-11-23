ab712c568de82c6b9392382c70636d80beb7159d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-11-23 19:49:51 -0300

    [ADD] dispatch number and lots on invoices

---
+++ l10n_ar_stock/models/account_invoice_line.py
@@ -0,0 +1,26 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    arba_code = fields.Char(
+    )
+    lot_ids = fields.Many2many(
+        'stock.production.lot',
+        related='move_line_ids.move_line_ids.lot_id',
+    )
+
+    @api.multi
+    @api.constrains('arba_code')
+    def check_arba_code(self):
+        for rec in self.filtered('arba_code'):
+            if len(rec.arba_code) != 6 or not rec.arba_code.isdigit():
+                raise UserError(_(
+                    'El código según nomenclador de arba debe ser de 6 dígitos'
+                    ' numéricos'))
--- l10n_ar_stock/models/stock_production_lot.py
+++ l10n_ar_stock/models/stock_production_lot.py
@@ -2,10 +2,12 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import res_company
-from . import stock_book
-from . import stock_picking
-from . import product_template
-from . import product_uom
-from . import account_invoice_line
-from . import stock_production_lot
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
+
+    dispatch_number = fields.Char(
+    )
