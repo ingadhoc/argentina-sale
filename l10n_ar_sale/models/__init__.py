2750f8928a6a9c600d978285d4fbc49a932bcc2a
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-01-30 17:51:47 -0300

    [IMP] create sale book and stock book when create a new company

--- l10n_ar_sale/models/__init__.py
+++ l10n_ar_sale/models/__init__.py
@@ -6,3 +6,4 @@ from . import res_company
 from . import sale_checkbook
 from . import sale_order
 from . import sale_order_line
+from . import account_chart_template
---
+++ l10n_ar_sale/models/account_chart_template.py
@@ -0,0 +1,48 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, api
+
+
+class AccountChartTemplate(models.Model):
+    _inherit = 'account.chart.template'
+
+    @api.multi
+    def _load_template(
+            self, company, code_digits=None, transfer_account_id=None,
+            account_ref=None, taxes_ref=None):
+        self.ensure_one()
+        if company.localization:
+            self.generate_sale_checkbook(company)
+        return super(AccountChartTemplate, self)._load_template(
+            company, code_digits, transfer_account_id,
+            account_ref, taxes_ref)
+
+    @api.model
+    def generate_sale_checkbook(
+            self, company):
+        checkbook_vals = self._prepare_all_checkbook_data(company)
+        self.check_created_checkbook(checkbook_vals, company)
+        return True
+
+    @api.model
+    def check_created_checkbook(self, checkbook_vals, company):
+        """
+        This method used for checking new checkbook already created or not.
+        If not then create new checkbook.
+        """
+        checkbook = self.env['sale.checkbook'].search([
+            ('name', '=', checkbook_vals['name']),
+            ('company_id', '=', company.id)])
+        if not checkbook:
+            checkbook.create(checkbook_vals)
+        return True
+
+    @api.model
+    def _prepare_all_checkbook_data(self, company):
+        vals = {
+            'name': 'Talonario Ventas (%s)' % (company.name),
+            'company_id': company.id,
+        }
+        return vals
