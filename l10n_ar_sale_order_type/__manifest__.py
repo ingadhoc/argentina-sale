2b38eb53957fcb1c6a8015ba39063162693b3505
nicomacr <nmr@adhoc.com.ar>
2019-04-15 15:28:25 -0300

    [FIX] l10n_ar_sale_order_type: remove the invisible for the secuence field in SO type

--- l10n_ar_sale_order_type/__manifest__.py
+++ l10n_ar_sale_order_type/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Sale with Sale Order Type',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
