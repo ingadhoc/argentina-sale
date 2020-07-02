5918400479533f084920164922a71bf97972cf03
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-07-01 22:15:55 -0300

    [IMP] l10n_ar_stock: remito report

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remito electrónico Argentino',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.2.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
