a8dded4c9f5ddc5cc228e6916b4c13b93a418216
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-01-14 14:47:01 -0300

    [IMP] l10n_ar_stock: move packages qty from _delivery

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remitos, COT y demas ajustes de stock para Argentina',
-    'version': '13.0.1.7.0',
+    'version': '13.0.1.9.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
