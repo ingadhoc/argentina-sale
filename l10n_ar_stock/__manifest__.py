0e9f398587b319b5314e486763ba084ba13f1c3e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-05-11 17:14:19 -0300

    [IMP] l10n_ar_stock: add mobile/phone Son útiles para los transportistas, para que puedan contactar al destinatario

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remitos, COT y demas ajustes de stock para Argentina',
-    'version': '13.0.1.14.0',
+    'version': '13.0.1.15.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
