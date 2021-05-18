c7ff3dbc48a545152769bda53adea0ad23ae77c9
Bruno Zanotti (ADHOC) <bz@adhoc.com.ar>
2021-05-18 13:10:46 -0300

    [FIX] l10n_ar_stock: show VAT on invoices report
    Steps to replicate the error:
    1. Create a partner with:
        - Responsibility type: "Cliente / Proveedor del Exterior"
        - Identification number: "VAT" with a random number.
    2. Create and print an invoice for this partner.
    
    Before this commit, the vat number was empty.

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remitos, COT y demas ajustes de stock para Argentina',
-    'version': '13.0.1.18.0',
+    'version': '13.0.1.19.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
