69c871e741dfed0d8d67c79d2fafc0cacbba83f1
nicolas <nmr@adhoc.com.ar>
2021-11-17 09:04:31 -0300

    [FIX] l10n_ar_sale*: change the over of Write to the correct module to fulfill with dependencies
    X-original-commit: 1b8a2ed5a1eb96ae7e1316433eaecd7bbde18338

--- l10n_ar_sale/models/sale_order.py
+++ l10n_ar_sale/models/sale_order.py
@@ -98,27 +98,6 @@ class SaleOrder(models.Model):
                 sale_checkbook.sequence_id._next() or _('New')
         return super(SaleOrder, self).create(vals)
 
-
-    def write(self, vals):
-        """A sale checkbook could have a different order sequence, so we could
-        need to change it accordingly"""
-        if self.env.user.has_group('l10n_ar_sale.use_sale_checkbook') and vals.get('sale_checkbook_id'):
-            sale_checkbook = self.env['sale.checkbook'].browse(vals['sale_checkbook_id'])
-            if sale_checkbook.sequence_id:
-                for record in self:
-                    if (
-                        record.state in {"draft", "sent"}
-                        and record.type_id.sequence_id != sale_checkbook.sequence_id
-                    ):
-                        new_vals = vals.copy()
-                        new_vals["name"] = sale_checkbook.sequence_id._next() or _('New')
-                        super(SaleOrder, record).write(new_vals)
-                    else:
-                        super(SaleOrder, record).write(vals)
-                return True
-        return super().write(vals)
-
-
     def _amount_by_group(self):
         for order in self:
             # Hacemos esto para disponer de fecha del pedido y cia para calcular
