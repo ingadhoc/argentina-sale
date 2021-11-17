72893e06cce4780abbbbdc9263c5efbb04b8871a
nicolas <nmr@adhoc.com.ar>
2021-11-17 09:04:31 -0300

    [FIX] l10n_ar_sale*: change the over of Write to the correct module to fulfill with dependencies

--- l10n_ar_sale_order_type/models/sale_order.py
+++ l10n_ar_sale_order_type/models/sale_order.py
@@ -14,3 +14,23 @@ class SaleOrder(models.Model):
             self.sale_checkbook_id = self.type_id.sale_checkbook_id
         else:
             return super(SaleOrder, self).set_sale_checkbook()
+
+
+    def write(self, vals):
+        """A sale checkbook could have a different order sequence, so we could
+        need to change it accordingly"""
+        if self.env.user.has_group('l10n_ar_sale.use_sale_checkbook') and vals.get('sale_checkbook_id'):
+            sale_checkbook = self.env['sale.checkbook'].browse(vals['sale_checkbook_id'])
+            if sale_checkbook.sequence_id:
+                for record in self:
+                    if (
+                        record.state in {"draft", "sent"}
+                        and record.type_id.sequence_id != sale_checkbook.sequence_id
+                    ):
+                        new_vals = vals.copy()
+                        new_vals["name"] = sale_checkbook.sequence_id._next() or _('New')
+                        super(SaleOrder, record).write(new_vals)
+                    else:
+                        super(SaleOrder, record).write(vals)
+                return True
+        return super().write(vals)
