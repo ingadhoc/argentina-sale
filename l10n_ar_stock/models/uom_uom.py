d83198f611c6c157d215e4716258d8c3778aad00
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-24 16:59:20 +0000

    [IMP] l10n_ar_stock: arba code configurable from uom categ
    closes ingadhoc/argentina-sale#88
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_stock/models/uom_uom.py
+++ l10n_ar_stock/models/uom_uom.py
@@ -10,3 +10,10 @@ class ProductUom(models.Model):
 
     arba_code = fields.Char(
     )
+
+    def action_arba_codes(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'http://www.arba.gov.ar/bajadas/Fiscalizacion/Operativos/TransporteBienes/Documentacion/20080701-TB-TablasDeValidacion.pdf',
+            'target': 'new'}
