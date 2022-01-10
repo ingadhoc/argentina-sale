62a5fa73fddcf6fc5de39f5fb597c77acbf5153b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-01-10 14:37:58 -0300

    [REM] l10n_ar_sale: discrmination vat company option

--- l10n_ar_sale/wizards/res_config_settings.py
+++ l10n_ar_sale/wizards/res_config_settings.py
@@ -4,10 +4,6 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    sale_allow_vat_no_discrimination = fields.Selection(
-        related='company_id.sale_allow_vat_no_discrimination',
-        readonly=False,
-    )
     group_price_unit_with_tax = fields.Boolean(
         "Unit Price w/ Taxes",
         implied_group='l10n_ar_sale.sale_price_unit_with_tax',
