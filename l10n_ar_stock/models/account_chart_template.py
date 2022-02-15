4330d866019bd499c3cdecdf7c3fd1ddca09377d
Virginia Bonservizi <vib@adhoc.com.ar>
2022-02-15 09:12:15 -0300

    Commit en argentina-sale

--- l10n_ar_stock/models/account_chart_template.py
+++ l10n_ar_stock/models/account_chart_template.py
@@ -7,7 +7,7 @@ from odoo import models, api
 
 class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
-
+    _inherit = 'account.chart.template'
     def _load_template(
             self, company, code_digits=None,
             account_ref=None, taxes_ref=None):
