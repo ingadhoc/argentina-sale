42785bf144003b1a9e19f6a41332ae259082fa13
Virginia Bonservizi <vib@adhoc.com.ar>
2022-12-15 14:23:13 +0000

    [FIX] l10n_ar_stock: report signature section adjustment
    X-original-commit: 37692615471b33e3506f05fcdfaf81dfda189611

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remitos, COT y demas ajustes de stock para Argentina',
-    'version': "15.0.1.4.0",
+    'version': "15.0.1.5.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
