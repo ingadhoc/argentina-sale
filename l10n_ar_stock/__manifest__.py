173d6602543848be23820a790f497ff7834e0c8e
Juan Ignacio Carreras <jc@adhoc.com.ar>
2023-10-26 10:27:40 -0300

    [FIX]l10n_ar_stock: now I2of5 cant be used
    X-original-commit: 3d154474aabf770ba30d20cddcd7057b05c4ef6b

--- l10n_ar_stock/__manifest__.py
+++ l10n_ar_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Remitos, COT y demas ajustes de stock para Argentina',
-    'version': "16.0.1.3.0",
+    'version': "16.0.1.4.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
