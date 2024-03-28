f4602946e96a94d4692d4486834d187eb8fbff88
docker-odoo <mem@adhoc.com.ar>
2024-03-28 15:03:11 +0000

    [FIX] l10n_ar_sale: tomamos en cuenta la sequencia del talonario de ventas
    Ticket: 72835

--- l10n_ar_sale_order_type/models/sale_order.py
+++ l10n_ar_sale_order_type/models/sale_order.py
@@ -16,21 +16,9 @@ class SaleOrder(models.Model):
             return super(SaleOrder, self).set_sale_checkbook()
 
 
-    def write(self, vals):
-        """A sale checkbook could have a different order sequence, so we could
-        need to change it accordingly"""
-        if self.env.user.has_group('l10n_ar_sale.use_sale_checkbook') and vals.get('sale_checkbook_id'):
-            sale_checkbook = self.env['sale.checkbook'].browse(vals['sale_checkbook_id'])
-            if sale_checkbook.sequence_id:
-                for record in self:
-                    if record.sale_checkbook_id != sale_checkbook and (
-                        record.state in {"draft", "sent"}
-                        and record.type_id.sequence_id != sale_checkbook.sequence_id
-                    ):
-                        new_vals = vals.copy()
-                        new_vals["name"] = sale_checkbook.sequence_id._next() or _('New')
-                        super(SaleOrder, record).write(new_vals)
-                    else:
-                        super(SaleOrder, record).write(vals)
-                return True
-        return super().write(vals)
+    @api.onchange('partner_id', 'sale_checkbook_id')
+    def set_order_sequence(self):
+        super(SaleOrder, self).set_order_sequence()
+        if not self.sale_checkbook_id.sequence_id and \
+            self.type_id.sequence_id:
+                self.name = self.type_id.sequence_id or _('New')
