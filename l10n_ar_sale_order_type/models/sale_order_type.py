8ad9300df8dd326e531ae76540eae9fd495c739e
fw-bot-adhoc <93535218+fw-bot-adhoc@users.noreply.github.com>
2024-09-06 05:50:35 -0300

    [FW][FIX] l10n_ar_sale_order_type: fix varios (#200)
    * [FIX] l10n_ar_sale: fix checkbook and sequence
    
    * [FIX] l10n_ar_sale_order_type: fix checkbook and sequence
    
    ---------
    
    Co-authored-by: Maximiliano Mezzavilla <mem@adhoc.com.ar>

--- l10n_ar_sale_order_type/models/sale_order_type.py
+++ l10n_ar_sale_order_type/models/sale_order_type.py
@@ -11,4 +11,12 @@ class SaleOrderType(models.Model):
     sale_checkbook_id = fields.Many2one(
         'sale.checkbook',
         check_company=True,
+        help='Si se define un checkbook se usara este para este type. Si no se define se dejara el que se tome por '
+        'defecto.'
     )
+
+    _sql_constraints = [
+        ('only_one_sequence_on_sale_order',
+          'CHECK((sequence_id IS null) OR (sale_checkbook_id IS null))',
+            'No pueden estar seteada una secuencia de entrada y un talonario de ventas al mismo tiempo')
+    ]
