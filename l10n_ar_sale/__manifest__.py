9e3fe805d1e78a0efb5243e720e8be0b7595af3c
Rocío Vega <rov@adhoc.com.ar>
2025-03-11 14:07:55 +0000

    [FIX] l10n_ar_sale: Fix in l10n_ar_sale_templates view preserved odoo ids
    X-original-commit: 4b0d8cd028706f7c150755d7fa0784fe0105da92

--- l10n_ar_sale/__manifest__.py
+++ l10n_ar_sale/__manifest__.py
@@ -1,4 +1,5 @@
 {
+<<<<<<< HEAD
     "name": "Argentinian Sale Total Fields",
     "version": "18.0.1.0.0",
     "category": "Localization/Argentina",
@@ -10,6 +11,31 @@
     "depends": [
         "sale",
         "l10n_ar_tax",
+||||||| parent of 877e1da (temp)
+    'name': 'Argentinian Sale Total Fields',
+    'version': "17.0.1.3.0",
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'sale',
+        'l10n_ar',
+=======
+    'name': 'Argentinian Sale Total Fields',
+    'version': "17.0.1.4.0",
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'sale',
+        'l10n_ar',
+>>>>>>> 877e1da (temp)
     ],
     "external_dependencies": {},
     "data": [
