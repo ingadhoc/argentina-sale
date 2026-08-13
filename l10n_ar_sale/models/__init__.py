573f6a011b77ed3a7727b1002ae17f5b263d9867
Camila Vives <cav@adhoc.com.ar>
2026-08-13 14:06:51 +0000

    [FIX] l10n_ar_sale: keep origin invoice reference on credit notes from SO
    When a sale order generates a credit note (because the amount left to invoice
    turned negative), the core only links it to its origin invoice when
    _refunds_origin_required() is True, which is not the case in AR. Without that
    link the document had no reference at all to the invoice it came from, so we
    at least set the invoice name on the Reference field.
    
    closes ingadhoc/argentina-sale#327
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- l10n_ar_sale/models/__init__.py
+++ l10n_ar_sale/models/__init__.py
@@ -2,5 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from . import account_move
 from . import sale_order
 from . import sale_order_line
